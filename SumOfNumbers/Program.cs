dbdbb05986f63c1d966becf88b0bcaf0cacff800
developer2 <developer2@DEVELOPER2>
2016-10-06 13:17:04 +0300

    SumOfNumbers with progresion

--- SumOfNumbers/Program.cs
+++ SumOfNumbers/Program.cs
@@ -6,14 +6,22 @@ namespace SumOfNumbers
     {
         static void Main(string[] args)
         {
-            int sum = 0;
+            int a1 = 3;
+            int an = 999;
+            int n = 999 / 3;
+            double sum3 = (a1 + an) / 2.0 * n;
 
-            for (int number = 1; number < 1000; number++)
-            {
-                if (number % 3 == 0 || number % 5 == 0)
-                    sum += number;
-            }
-            Console.WriteLine("Sum of all numbers less than 1000 multiplies 3 or 5 = " + sum);
+            a1 = 5;
+            an = 995;
+            n = 999 / 5;
+            double sum5 = (a1 + an) / 2.0 * n;
+
+            a1 = 15;
+            an = 990;
+            n = 999 / 15;
+            double sum15 = (a1 + an) / 2.0 * n;
+
+            Console.WriteLine("Sum of all numbers less than 1000 multiplies 3 or 5 = " + (int)(sum3 + sum5 - sum15));
         }
     }
 }
